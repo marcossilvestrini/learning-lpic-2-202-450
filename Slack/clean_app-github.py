2ad8e6da6f4540d601f808eb4f786aa5e78ff4d4
marcossilvestrini <marcos.silvestrini@gmail.com>
2023-02-20 13:28:46 -0300

    Fix slack cleanup channel error

--- Slack/clean_app-github.py
+++ Slack/clean_app-github.py
@@ -1,5 +1,5 @@
 from slack_cleaner2 import SlackCleaner
 from silvestrini import *
 s = SlackCleaner(TOKEN, sleep_for=1)
-for msg in s.c.general.msgs(with_replies=True):
+for msg in s.c.appgithub.msgs(with_replies=True):
   msg.delete()
--- Slack/clean_bot.py
+++
@@ -1,6 +0,0 @@
-from silvestrini import *
-from slack_cleaner2 import SlackCleaner
-s = SlackCleaner(TOKEN, sleep_for=1)
-for msg in s.c['auto-build'].msgs():
-  if msg.bot:
-    msg.delete()
