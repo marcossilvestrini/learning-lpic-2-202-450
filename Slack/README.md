2f035224dcf1dcb929ad2bcf03ce24df58f05805
marcossilvestrini <marcos.silvestrini@gmail.com>
2023-02-20 11:22:55 -0300

    Update slack scripts

---
+++ Slack/README.md
@@ -0,0 +1,9 @@
+# Docs
+
+## Oficial page
+
+<https://github.com/sgratzl/slack_cleaner2>
+
+## Examples
+
+<https://github.com/sgratzl/slack-cleaner/issues/79>
--- Slack/delete-template.py
+++
@@ -1,8 +0,0 @@
-from slack_cleaner2 import *
-token = "foobarbeer"
-channel = "name-of-your-channel"
-s = SlackCleaner(token)
-for msg in s.msgs(filter(match(channel), s.conversations)):
-  print(msg)
-  # delete messages, its files, and all its replies (thread)
-  # msg.delete(replies=True, files=True)
