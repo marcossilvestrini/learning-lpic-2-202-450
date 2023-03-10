47ed3083861ec12ce216f5a5a0531b4f6d4040cd
marcossilvestrini <marcos.silvestrini@gmail.com>
2023-03-09 22:54:36 -0300

    Update files for class today

--- Vagrant/HTTP/scripts/user-data-ol9-apache-ha.sh
+++ Vagrant/HTTP/scripts/user-data-ol9-apache-ha.sh
@@ -50,23 +50,24 @@ cp configs/commons/app.pl /var/www/perl/
 dos2unix /var/www/perl/app.pl
 chmod -R 755 /var/www/perl/app.pl
 
+# Create user autentication db for sites
+mkdir /etc/httpd/htpasswd
+echo "vagrant" | htpasswd -c -i /etc/httpd/htpasswd/.htpasswd vagrant
+echo "silvestrini" | htpasswd -i /etc/httpd/htpasswd/.htpasswd silvestrini
+echo "lpic2" | htpasswd -i /etc/httpd/htpasswd/.htpasswd lpic2
+cp  configs/commons/.htgroup /etc/httpd/htpasswd
+dos2unix /etc/httpd/htpasswd/.htgroup
+
 # Create sites for autentication rules
 mkdir {/var/www/html/topsecret,/var/www/html/admin}
 touch /var/www/html/topsecret/topsecret_file{1..3}
 touch /var/www/html/admin/admin_file{1..3}
 
-# Set user autentication for sites
-echo "vagrant" | htpasswd -c -i /var/www/html/topsecret/.htpasswd vagrant
-echo "silvestrini" | htpasswd -i /var/www/html/topsecret/.htpasswd silvestrini
-echo "lpic2" | htpasswd -i /var/www/html/topsecret/.htpasswd lpic2
-
 # Set sites for autentication rules(mod_authn)
 cp configs/commons/.htaccess /var/www/html/topsecret
-cp configs/commons/.htaccess /etc/httpd/logs
+cp configs/commons/.htaccess /var/log/httpd
 dos2unix /var/www/html/topsecret/.htaccess
-dos2unix /etc/httpd/logs/.htaccess
-cp configs/commons/.htgroup /var/www/html/topsecret
-dos2unix /var/www/html/topsecret/.htgroup
+dos2unix /var/log/httpd/.htaccess
 
 # Set sites for autorization rules (mod_authz)
 cp configs/commons/.htaccess_admin /var/www/html/admin/.htaccess
@@ -90,7 +91,7 @@ rm /etc/ssl/certs/lpic2*
 openssl req -new -nodes -newkey rsa:4096 \
 -passout pass:vagrant \
 -subj "/C=BR/ST=SaoPaulo/L=SaoPaulo/O=Silvestrini Inc. /OU=IT Department/CN=lpic2.com.br" \
--keyout /etc/ssl/certs/lpic2.com.br.key -out /etc/ssl/certs/lpic2.com.br.csr
+-keyout /etc/ssl/certs/lpic2.com.br.key -out /etc/ssl/certs/lpic2.com.br.csr 2>/dev/null
 
 ## Check crs file
 openssl req -in /etc/ssl/certs/lpic2.com.br.csr -text -noout
@@ -99,7 +100,7 @@ openssl req -in /etc/ssl/certs/lpic2.com.br.csr -text -noout
 openssl req -new -x509 -days 30 -nodes -newkey rsa:4096 \
 -passout pass:vagrant \
 -subj "/C=BR/ST=Sao Paulo/L=Sao Paulo/O=Silvestrini Inc. /OU=IT Department/CN=lpic2.com.br" \
--keyout /etc/ssl/certs/lpic2.com.br.key -out /etc/ssl/certs/lpic2.com.br.crt
+-keyout /etc/ssl/certs/lpic2.com.br.key -out /etc/ssl/certs/lpic2.com.br.crt 2>/dev/null
 
 ## Generate client certificate
 openssl pkcs12 -password pass:vagrant  -export -in /etc/ssl/certs/lpic2.com.br.crt  \
